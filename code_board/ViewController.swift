1ccc40422dddd27da1ac42b7743aa52805f8455d
TripleJ709 <ijn9975@naver.com>
2025-06-21 19:17:01 +0900

    start

---
+++ code_board/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  code_board
+//
+//  Created by 장주진 on 6/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
