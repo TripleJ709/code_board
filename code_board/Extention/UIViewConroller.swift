c518a50ecd21c080c47d50c58251dc159c738cac
TripleJ709 <ijn9975@naver.com>
2025-06-28 19:30:03 +0900

    create posts

---
+++ code_board/Extention/UIViewConroller.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewConroller.swift
+//  code_board
+//
+//  Created by 장주진 on 6/28/25.
+//
+
+import UIKit
+
+extension UIViewController {
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default))
+        self.present(alert, animated: true)
+    }
+}
